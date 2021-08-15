c0d8e5f5b200ffe10b149a5b642076cb431bf1f9
Fathur Walkers <fathurwalkers44@gmail.com>
2021-08-14 21:54:19 -0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Artikel;
+use App\Models\Login;
+use App\Models\Kategori;
+use App\Models\ArtikelKategori;
+use App\Models\Gambar;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //
     }
 }
