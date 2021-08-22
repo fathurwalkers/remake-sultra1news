8a3aa3b1b30ab0f5c357740066a92c68818408f6
Fathur Walkers <fathurwalkers44@gmail.com>
2021-08-21 22:01:41 -0700

    update

--- resources/views/dashboard/tambah-artikel.blade.php
+++ resources/views/dashboard/tambah-artikel.blade.php
@@ -46,13 +46,13 @@
                                         <div class="col-lg-12 col-sm-12 col-md-12">
                                             <div class="input-group mb-3">
                                                 <div class="input-group-prepend">
-                                                  <label class="input-group-text" for="inputGroupSelect01">Kategori</label>
+                                                    <label class="input-group-text" for="inputGroupSelect01">Kategori</label>
                                                 </div>
                                                 <select class="custom-select" id="inputGroupSelect01">
-                                                  <option selected>Choose...</option>
-                                                  <option value="1">One</option>
-                                                  <option value="2">Two</option>
-                                                  <option value="3">Three</option>
+                                                    <option selected>Choose...</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
                                                 </select>
                                               </div>
                                         </div>
