ec365b93bf20ea61a97f45250a812a6d84270c3e
Fathur Walkers <fathurwalkers44@gmail.com>
2021-08-21 18:40:49 -0700

    update

--- resources/views/dashboard/tambah-artikel.blade.php
+++ resources/views/dashboard/tambah-artikel.blade.php
@@ -3,7 +3,17 @@
 @section('main-section')
     <div class="card">
         <div class="card-header bg-dark text-white text-bold">
-            Semua Postingan
+            Tambah Postingan 
+            <a class="btn btn-danger btn-sm float-right" href="{{ route('daftar-artikel') }}" role="button">Kembali</a>
+        </div>
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 col-md-12">
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
