c86696f00b3252276b34487b3bea9ddd379ae370
Fathur Walkers <fathurwalkers44@gmail.com>
2021-08-27 12:34:55 -0700

    update

--- database/migrations/2021_08_15_043103_create_artikels_table.php
+++ database/migrations/2021_08_15_043103_create_artikels_table.php
@@ -14,7 +14,7 @@ class CreateArtikelsTable extends Migration
             $table->longText('artikel_isi');
             $table->string('artikel_slug');
             $table->string('artikel_status'); // PUBLISHED - DRAFT - TRASH
-            $table->string('artikel_dibuat');
+            $table->date('artikel_dibuat');
             $table->unsignedBigInteger('login_id')->nullable();
             $table->foreign('login_id')->references('id')->on('login');
             $table->string('artikel_headergambar')->nullable();
